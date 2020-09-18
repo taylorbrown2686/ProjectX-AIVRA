60f065f84c76a8652a84059f43a9eda832c1f7eb
Mahnoor Naeem <mahnoor.naeem@werplay.com>
2020-09-19 00:25:13 +0500

    Avatar Script

---
+++ ProjectX/Assets/_Scripts/Avatar.cs
@@ -0,0 +1,18 @@
+﻿// Avatar Class for ProjectX
+
+public class Avatar 
+{
+
+    public class FacialFeatures {
+
+        public int eye;
+        public int nose;
+        public int mouth;
+        public int hair;
+        public int facialShape;
+
+    }
+
+
+    public int body;
+}
