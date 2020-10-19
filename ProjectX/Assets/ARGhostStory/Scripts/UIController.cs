39bf39b65e55ed70e7c89b7c83ba3fa5edcf8639
Mahnoor Naeem <mahnoor.naeem@werplay.com>
2020-10-20 01:27:01 +0500

    commit 1

---
+++ ProjectX/Assets/ARGhostStory/Scripts/UIController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : Singleton<UIController>
+{
+    public GameObject namePanel;
+    public InputField nameInputField;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("Name"))
+        {
+            namePanel.SetActive(true);
+        }
+        else {
+            namePanel.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nameInputCallback() {
+        string name = nameInputField.text.Trim();
+        if (!string.IsNullOrEmpty(name)){
+            PlayerPrefs.SetString("Name", name);
+            namePanel.SetActive(false);
+        }
+    }
+}
